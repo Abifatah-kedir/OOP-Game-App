6d0722f1605afe54499e6be156f3c668fed62247
codenerd1 <30376311+kedir1@users.noreply.github.com>
2019-06-13 14:07:22 -0500

    step 4 Done.
    added a function that uses a loop, in the loop creates listitems,  append them into their UL parent, add a class 'letter' each listems after appending them.

--- app.js
+++ app.js
@@ -20,17 +20,37 @@ overleyElement.addEventListener('click', () =>
 
 // varia
 const phrasesInArray = [
-    'variables',
-    'loops',
-    'functions',
-    'operators',
+    'Variables',
+    'Loops',
+    'Functions',
+    'Operators',
     'Arrays'
 ];
 
 // reusable function that randomly picks phrase from passed variable container.
-function getRandomPhraseAsArray(arr){
+function getRandomPhraseAsArray(arr)
+{
+    //do stuff to any arr that is passed in
     let  getRandomPhrase= arr[Math.floor(Math.random()* arr.length)];
     let  phraseSplit = getRandomPhrase.split('');
     return phraseSplit;
 }
-getRandomPhraseAsArray(phrasesInArray);// calls the function by passing variable constainer.
+// calls the function by passing variable constainer.
+const phraseArray = getRandomPhraseAsArray(phrasesInArray);
+
+
+function addPhraseToDisplay(arr)
+{
+    // do stuff any arr that is passed in, and add to `#phrase ul`
+    for(let i = 0; i < arr.length; i++)
+    {
+        const parentUl = document.querySelector('ul');
+        const listItem = document.createElement('li');
+        listItem.innerHTML += arr[i];
+        parentUl.appendChild(listItem);
+        listItem.className = 'letter';
+
+    }
+
+}
+addPhraseToDisplay(phraseArray);
