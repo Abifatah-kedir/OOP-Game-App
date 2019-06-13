a01615e8b13b56d7eca29f3f0853ce09ce57e0d1
codenerd1 <30376311+kedir1@users.noreply.github.com>
2019-06-13 00:11:41 -0500

    Done for first three steps
    selected major elements and stored created variables.

---
+++ app.js
@@ -0,0 +1,29 @@
+// used variables.
+const qwertyElement = document.getElementById('qwerty');
+const phraseElement = document.getElementById('phrase');
+
+//counter.
+let missed = 0;
+
+//selects  the start button and stores back to the 
+// overleyElement variable
+const overleyElement = document.querySelector('.btn__reset');
+
+
+//triggers the 'click' on start game button to hide the overley.
+overleyElement.addEventListener('click', () => 
+{
+    const hideSreenOverley = document.querySelector('.start');
+    hideSreenOverley.style.display = 'none';
+});
+
+
+// varia
+const phrasesInArray = [
+    'variables',
+    'loops',
+    'functions',
+    'operators',
+    'Arrays'
+];
+
