f1bac4d2a508d796aadd148f4f649d002d85dab7
codenerd1 <30376311+kedir1@users.noreply.github.com>
2019-06-18 20:07:26 -0500

    Mostly Done!
    Created array of string for the phrase class, added random number that lets unknown phrase  chosen when the page loads and appending list phrases. hard coded as letter cannot be clicked twice.
    if the player makes wrong guesses, he/she loses, if no wins.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // used variables.
-const qwertyElement = document.getElementById('qwerty');
+const qwertyElement = document.querySelector('#qwerty');
 const phraseElement = document.getElementById('phrase');
+
+const loosingTries = document.querySelectorAll(".tries");
 
 //counter.
 let missed = 0;
@@ -9,7 +11,6 @@ let missed = 0;
 // overleyElement variable
 const overleyElement = document.querySelector('.btn__reset');
 
-
 //triggers the 'click' on start game button to hide the overley.
 overleyElement.addEventListener('click', () => 
 {
@@ -17,14 +18,12 @@ overleyElement.addEventListener('click', () =>
     hideSreenOverley.style.display = 'none';
 });
 
-
-// varia
 const phrasesInArray = [
-    'Variables',
-    'Loops',
-    'Functions',
-    'Operators',
-    'Arrays'
+    'take the risk',
+    'lose the chance',
+    'prove them wrong',
+    'Dream big pray bigger',
+    'if you want it work for it'
 ];
 
 // reusable function that randomly picks phrase from passed variable container.
@@ -37,20 +36,100 @@ function getRandomPhraseAsArray(arr)
 }
 // calls the function by passing variable constainer.
 const phraseArray = getRandomPhraseAsArray(phrasesInArray);
-
-
+  
 function addPhraseToDisplay(arr)
 {
     // do stuff any arr that is passed in, and add to `#phrase ul`
-    for(let i = 0; i < arr.length; i++)
+    for(let i = 0; i < arr.length; i += 1)
     {
-        const parentUl = document.querySelector('ul');
+        const parentUl = document.querySelector('#phrase ul');
         const listItem = document.createElement('li');
         listItem.innerHTML += arr[i];
         parentUl.appendChild(listItem);
-        listItem.className = 'letter';
 
+        if( /*arr[i] !== ' ' &&*/  arr[i].match(/^[a-zA-Z]+$/) )
+        {
+            listItem.className = 'letter';
+        }
     }
-
+    return arr;
 }
+
 addPhraseToDisplay(phraseArray);
+
+let matchFoud = false;
+
+function checkLetter(playerClickedButton)
+{
+    let  matchFoud = null;
+    const letterList = document.querySelectorAll('.letter');
+
+    for (let i = 0; i < letterList.length; i += 1) 
+    {
+        const letter = letterList[i].textContent.toLowerCase();
+        if (playerClickedButton === letter)
+        {
+            matchFoud = true;
+            letterList[i].classList.add('show');
+        }
+    }
+    return matchFoud;  
+}
+
+
+qwertyElement.addEventListener('click', (event) =>
+{
+    if (event.target.tagName === 'BUTTON')
+    {
+        const letterClicked = event.target.textContent;
+
+        event.target.classList.add("chosen");
+        event.target.setAttribute('disabled', true);
+        let letterFound =  checkLetter(letterClicked);
+
+       if (letterFound === null)
+       {
+            loosingTries[missed].style.display = 'none';
+            missed += 1;
+       }
+    }
+    checkWin();
+     
+});
+
+/*
+      =================================
+        checks if the player wins or loses.
+       ===================================
+*/
+
+   function checkWin() 
+   {
+       const lettersWithShow = document.querySelectorAll('.show');
+       const lettersWithLetter = document.querySelectorAll('.letter');
+   
+       const showOverley = document.getElementById('overlay');
+       const title = document.querySelectorAll('.title');
+
+       if (missed >= 5) 
+       {
+           showOverley.classList.add('lose');
+           console.log("oops, you miss letters")
+           title.textContent = 'Sorry, you did loose';
+       }
+       else if ( lettersWithShow.length === lettersWithLetter.length)
+       {
+           showOverley.classList.add('win');
+           console.log("wax walba oo oki.")
+           title.textContent = 'Congrats!, you win';
+       }
+   }
+  
+ 
+
+
+
+
+
+
+
