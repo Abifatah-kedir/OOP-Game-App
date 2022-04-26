3c7dee7151d6f2d98fa3f004fa76b768009b5379
abifatah <abifatah.kedir@gmail.com>
2022-04-26 10:13:00 -0500

    added new methods to seperate the job

--- js/Game.js
+++ js/Game.js
@@ -23,6 +23,7 @@ class Game {
             ];
         return createdPhrase;
     };
+
     // /**
     //     * Selects random phrase from phrases property.
     //     * @return {Object} Phrase object chosen to be used.
@@ -37,11 +38,7 @@ class Game {
     * Checks for winning move
     * @return {boolean} True if game has been won, false if game wasn't won
     */
-
     checkWin() {
-       
-        // const gameStatus = null;
-
         const lettersWithShow = document.querySelectorAll('.show');
         const lettersWithLetter = document.querySelectorAll('.letter');
         const gameSection = document.querySelectorAll('.section');
@@ -83,13 +80,6 @@ class Game {
         }else {
             console.log("game is not done yet");
         }
-
-        // for (let i =0; i < lettersWithLetter.length; i++){
-        //     if (lettersWithLetter[i].classList.contains("show") ) {
-        //         console.log(lettersWithLetter[i]);
-        //     }
-        // }
-       
     }
 
     // /**
@@ -98,28 +88,33 @@ class Game {
     //     * Checks if player has remaining lives and ends game if player is out
     // */
     removeLife(){
-       
         const loosingTries = document.querySelectorAll(".tries img");
-        console.log(loosingTries[loosingTries.length - 1]);
-        // const theHearts = loosingTries[loosingTries.length - 1];
-        // theHearts.src = "images/lostHeart.png";
+        const theHearts = loosingTries[this.missed];
+        theHearts.src = "images/lostHeart.png";
+        
+        this.missed +=1;
 
-        // if(this.missed == 5) {
-        //     gameOver(); 
-        // }
+        if(this.missed == 5) {
+            this.gameOver(this.missed); 
+            console.log("All hearts are gone")
+        }
     }
     // /**
     //     * Displays game over message
     //     * @param {boolean} gameWon - Whether or not the user won the game
     // */
     gameOver(gameWon) {
-        const loseMessage = document.createElement("h1");
-        loseMessage.textContent = "Sorry, you lose! "; 
-        const winMessage = document.createElement('h1');
-        winMessage.textContent = "Congrats!, you win ";
+       document.querySelector('.start').style.display = "block";
+       const winMessage = document.getElementById("game-over-message");
+       console.log();
+        if (gameWon) {
+            winMessage.parentElement.className = "win";
+            winMessage.textContent = "Great job!";
+        } else {
+            winMessage.parentElement.className = "lose";
+            loseMessage.textContent = "Sorry, better luck next time!"; 
+        }
     }
-
-
 
     // /**
     //     * Begins game by selecting a random phrase and displaying it to user
